d2d9570b24a7833ac815f6084ca2226bcc0b1229
VladChipurko <87361945+VladChipurko@users.noreply.github.com>
2023-10-01 23:53:51 +0300

    Add Producers

--- BlockingQueue/src/controller/BlockingQueueAppl.java
+++ BlockingQueue/src/controller/BlockingQueueAppl.java
@@ -7,15 +7,24 @@ import tasks.Produser;
 
 public class BlockingQueueAppl {
 
+	private static final int PROD_NUMS = 10;
+
 	public static void main(String[] args) {
 		BlockingQueue<String> queue = new ArrayBlockingQueue<>(10);
 		
-		Thread produserThread = new Thread(new Produser(queue));
+		Thread[] arr = new Thread[PROD_NUMS];
+		
+		for (int i = 0; i < PROD_NUMS; i++) {
+			arr[i] = new Thread(new Produser(queue, i));
+			arr[i].start();
+		}
+		
+		//Thread produserThread = new Thread(new Produser(queue));
 		Thread consumerThread = new Thread(new Consumer(queue));
 		
 		consumerThread.setDaemon(true);
 		
-		produserThread.start();
+		//produserThread.start();
 		consumerThread.start();
 
 	}
--- BlockingQueue/src/tasks/Produser.java
+++ BlockingQueue/src/tasks/Produser.java
@@ -4,16 +4,18 @@ import java.util.concurrent.BlockingQueue;
 public class Produser implements Runnable {
 	
 	private final BlockingQueue<String> queue;
+	private int number;
 	
-	public Produser(BlockingQueue<String> queue) {
+	public Produser(BlockingQueue<String> queue, int i) {
 		this.queue = queue;
+		this.number = i;
 	}
 
 	@Override
 	public void run() {
 		try {
 			for (int i = 0; i < 10; i++) {
-				String message = "Message " + i;
+				String message = "Message number " + i + ", produser " + number;
 				queue.put(message);
 				System.out.println(message);
 				Thread.sleep(1000);
